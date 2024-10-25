65255f96ff600c9982258f33898ae33f884efdd8
Charles Caleb <charlesofficial9@gmail.com>
2024-10-25 13:49:42 +0100

    update

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const toggleButton = document.querySelector(".nav-toggle-btn");
-const navBar = document.getElementById("nav-bar");
-const div1 = document.querySelector(".div-1");
-const div2 = document.querySelector(".div-2");
-const div3 = document.querySelector(".div-3");
-const div4 = document.querySelector(".div-4");
+const navBar = document.getElementById("nav-bar-hidden");
 
 function toggleNavBar() {
   const isHidden = window.getComputedStyle(navBar).display === "none";
-  // nav.classList.toggle("active");
-  div1.classList.toggle("logoActive");
-  div2.classList.toggle("logoActive");
-  div3.classList.toggle("logoActive");
-  div4.classList.toggle("logoActive");
 
   if (isHidden) {
-    navBar.style.display = "flex";
-    navBar.style.position = "absolute";
-    navBar.style.top = "0";
-    navBar.style.left = "0";
-    navBar.style.boxShadow = "5px 5px 5px rgba(0, 0, 0, 0.5)";
-
     if (window.innerWidth <= 1024) {
       document.addEventListener("click", handleOutsideClick);
     }
+    navBar.style.display = "flex";
   } else {
     navBar.style.display = "none";
     document.removeEventListener("click", handleOutsideClick);
@@ -42,5 +28,5 @@ function handleOutsideClick(event) {
 }
 
 window.addEventListener("resize", () => {
-  location.reload();
+  navBar.style.display = "none";
 });
